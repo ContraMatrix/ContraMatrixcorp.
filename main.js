e1e0cb7e76eb8d5e95411828a7980ab7cc292bb4
ContraMatrix Corp <141868658+ContraMatrix@users.noreply.github.com>
2023-08-16 20:25:51 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function openPopup(){
+   const popup=
+   document.querySelector(".popup");
+      popup.style.display = "block";
+}
+function closePopup(){
+    const popup=
+    document.querySelector(".popup");
+       popup.style.display = "none";
+ }
+
